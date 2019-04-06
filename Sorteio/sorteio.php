693bda831a5b92bac832bd0bb49ac418785173e0
Marcos Vinicius <agenciadeveloper2017@gmail.com>
2019-04-06 00:17:19 -0300

    Página sorteio por numero add.

--- Sorteio/sorteio.php
+++ Sorteio/sorteio.php
@@ -8,7 +8,18 @@
 </head>
 <body>
 
-	<?php $sorteado = "Fulano";?>
+	<?php
+
+	if (isset($_POST['num1']) && isset($_POST['num2'])) {
+		$num1 = $_POST['num1'];
+		$num2 = $_POST['num2'];
+
+		$sorteado = rand($num1, $num2); 
+	}
+
+	
+
+	?>
 
 
 
@@ -16,24 +27,24 @@
 		<h1>Sorteio MV</h1>
 	</header>
 	<section>
-		<form>
-			<a href="sorteio.php"><div><h2>Sortear por número</h2></div></a>
+		<form method="POST">
+			<a href="index.php"><div><h2>Sortear por nome</h2></div></a>
 			<div class="box-center">
 				<div class="box">
 					<div class="cir-img">
 						<img src="image/trofeu.png" width="89" height="89">
 					</div>
 					<div class="botao-p">
-						<?php $sorteado = "Fulano"; echo $sorteado; ?>
+						<?php if (isset($sorteado)) { echo $sorteado; } ?>
 					</div>
 				</div>
 				<div class="box">
 					<div class="box-rec">
 						<div class="botao-p ">
-							<input type="text" name="name"><div>Add</div>
+							<input type="text" name="num1"><div>Nº1</div>
 						</div>
 						<div class="botao-p ">
-							<?php $sorteado = "Fulano"; echo $sorteado; ?>
+							<input type="text" name="num2"><div>Nº2</div>
 						</div>
 					</div>
 					<div class="botao-p enviar">
